f23de478aac9ff897ea19804b928c3a456ded9b8
WittBulter <nanazuimeng123@gmail.com>
2016-10-17 18:54:08 +0800

    加入列表

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {MaterialModule} from '@angular/material'
 import {AppComponent} from './app.component'
 import {WelcomeComponent} from './welcome/welcome.component'
 
-import {routing, appRoutingProviders} from './app.routing'
+import {routing, appRoutingProviders} from './app.routing';
+import { MenuComponent } from './menu/menu.component'
 
 @NgModule({
     declarations: [
         AppComponent,
-        WelcomeComponent
+        WelcomeComponent,
+        MenuComponent
     ],
     imports: [
         BrowserModule,
