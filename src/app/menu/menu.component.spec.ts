c2f9b67d67908d331fdefb0c2c323c72a574dc9e
WittBulter <nanazuimeng123@gmail.com>
2016-10-18 17:24:41 +0800

    添加菜单

---
+++ src/app/menu/menu.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MenuComponent } from './menu.component';
+
+describe('Component: Menu', () => {
+  it('should create an instance', () => {
+    let component = new MenuComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+    }
+
+    menuTitles = [{
+        name: '归档',
+        detail: '',
+        number: '5'
+    }, {
+        name: ' 作者',
+        detail: '',
+        number: '5'
+    }, {
+        name: '搜索',
+        detail: '',
+        number: '5'
+    }]
+
+}
