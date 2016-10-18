0e75e786d15f166cea00b2f55ceae8ce543753b0
WittBulter <nanazuimeng123@gmail.com>
2016-10-18 18:12:51 +0800

    加入欢迎界面

---
+++ src/app/welcome/welcome.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { WelcomeComponent } from './welcome.component';
+
+describe('Component: Welcome', () => {
+  it('should create an instance', () => {
+    let component = new WelcomeComponent();
+    expect(component).toBeTruthy();
+  });
+});
