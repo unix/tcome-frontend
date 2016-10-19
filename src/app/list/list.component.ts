261edc2cc302e954ecf2ec92d31cd21433f6af21
WittBulter <nanazuimeng123@gmail.com>
2016-10-19 20:11:04 +0800

    增加客户端逻辑

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {List} from './list'
+import {ListService} from './list.service'
 
 @Component({
-  selector: 'app-list',
-  templateUrl: 'list.component.html',
-  styleUrls: ['list.component.scss']
+    selector: 'app-list',
+    templateUrl: 'list.component.html',
+    styleUrls: ['list.component.scss'],
+    providers: [ListService]
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+    constructor(private listService: ListService) {}
 
-  ngOnInit() {
-  }
+    list: List[]
+    errorMessage: string
+
+    ngOnInit() {
+        this.getList()
+    }
+
+    getList() {
+        this.listService.getList()
+            .subscribe(
+                list => this.list = list,
+                error => this.errorMessage = error
+            )
+    }
 
 }
--- src/app/list/list.service.ts
+++ src/app/list/list.service.ts
@@ -1,10 +1,34 @@
 import {Injectable} from '@angular/core'
+import { Http, Response } from '@angular/http';
 import {Observable} from 'rxjs/Observable'
+import {Headers, RequestOptions} from '@angular/http';
+
+import List from './list'
 
 @Injectable()
-export class listService {
+export class ListService {
 
-    constructor() {
+    constructor(private http: Http) {
+    }
+
+    private listUrl = 'http://127.0.0.1/article'
+
+    getList(): Observable<List[]> {
+        return this.http.get(this.listUrl)
+            .map(this.extractData)
+            .catch(this.handleError)
+    }
+
+    private extractData(res: Response) {
+        const body = res.json();
+        return body || {}
+    }
+
+    private handleError (error: any) {
+        const errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg);
+        return Observable.throw(errMsg)
     }
 
 
