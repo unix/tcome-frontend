eb1e789b00f71e3f345f90559bc02787aeeb12c9
WittBulter <nanazuimeng123@gmail.com>
2016-10-19 19:36:01 +0800

    增加listService

---
+++ src/app/services/list/list.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import {TestBed, async, inject} from '@angular/core/testing';
+import {ListService} from './list.service';
+
+describe('Service: ListService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ListService]
+        });
+    });
+
+    it('should ...', inject([ListService], (service: ListService) => {
+        expect(service).toBeTruthy();
+    }));
+});
