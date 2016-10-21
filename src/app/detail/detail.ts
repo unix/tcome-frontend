b617f38e354086a147e4e296f381eb33bd5bbd8d
WittBulter <nanazuimeng123@gmail.com>
2016-10-21 15:18:48 +0800

    构建详情模型

---
+++ src/app/detail/detail.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by WittBulter on 2016/10/21.
+ * Detail Model
+ */
+
+export class Detail {
+    constructor(
+        public id: string,              // id
+        public title: string,           // 标题
+        public content: string,         // 详情
+        public createdAt: string,       // 创建日期
+        public updatedAt: string,       // 更新日期
+        public tags: string[],          // 标签
+        public readTotal: number,       // 阅读数
+        public commentTotal: number,    // 评论数
+    ){}
+}
