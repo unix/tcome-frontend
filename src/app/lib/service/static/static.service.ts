ccbd309ae9b7b0963aa634c09159ee815856e024
WittBulter <nanazuimeng123@gmail.com>
2016-12-10 22:56:21 +0800

    add staging

--- src/app/lib/service/static/static.service.ts
+++ src/app/lib/service/static/static.service.ts
@@ -7,9 +7,10 @@ export class StaticService {
     }
 
     private server: string = 'http://admin.itsmycar.cn/'
+    private staging: string = 'http://127.0.0.1:1337/'
 
     public makeApi(path: string) {
-        return this.server + path
+        return this.staging + path
     }
 
 }
