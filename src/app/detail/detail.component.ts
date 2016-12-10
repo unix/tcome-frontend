13d0c9aa0443b0f0a8199068b0c2ea496fa04b70
WittBulter <nanazuimeng123@gmail.com>
2016-12-11 00:11:33 +0800

    完善详情路由

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,19 +1,44 @@
 import {Component, OnInit} from '@angular/core'
+import {Router, ActivatedRoute, Params} from '@angular/router'
+import {Title} from '@angular/platform-browser'
+
+import {StaticService} from '../lib/service/static'
+import {DetailService} from './detail.service'
+import {Detail} from './detail'
+
 let a =  require('simplemde')
 
 @Component({
     selector: 'app-detail',
     templateUrl: './detail.component.html',
-    styleUrls: ['./detail.component.scss']
+    styleUrls: ['./detail.component.scss'],
+    providers: [StaticService, DetailService]
 })
 export class DetailComponent implements OnInit {
 
-    constructor() {
+    constructor(
+        private detailService: DetailService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private titleService: Title,
+    ) {
+    }
+    public detail: Detail
+
+    getDetail (id: string){
+        this.detailService.getDetail(id)
+            .subscribe(
+                detail => this.detail = detail,
+                error => console.log(error)
+            )
     }
 
     ngOnInit() {
-        console.log(a);
-        // let simplemde = new SimpleMDE()
+        this.titleService.setTitle('文章详情-维特博客')
+        this.route.params.forEach((params: Params) => {
+            const id = params['id']
+            if (id) this.getDetail(id)
+        })
     }
 
 }
--- src/app/detail/detail.service.ts
+++ src/app/detail/detail.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http'
+import {Observable} from 'rxjs/Observable'
+
+import {StaticService} from '../lib/service/static'
+import {Detail} from './detail'
 
 @Injectable()
 export class DetailService {
 
-  constructor() { }
+    constructor(private http: Http,
+                private staticService: StaticService) {
+    }
+
+    private detailUrl = this.staticService.makeApi('articles')
+    private options = new RequestOptions({
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        }),
+    })
+
+    getDetail (id: string): Observable<Detail> {
+        return this.http.get(`${this.detailUrl}/${id}`, this.options)
+            .map(this.extractData)
+            .catch(this.handleError)
+    }
+
+
+    private extractData(res: Response) {
+        const body = res.json()
+        return body || {}
+    }
+
+    private handleError(error: any) {
+        if (error instanceof Response) {
+            return Observable.throw(error.json().message || '服务器错误');
+        }
+        return Observable.throw(error || '服务器错误')
+    }
 
 }
