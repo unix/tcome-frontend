a13c7fbe18e3f3347574aa0acca3298a5a510aba
WittBulter <nanazuimeng123@gmail.com>
2016-12-10 23:49:55 +0800

    日期格式优化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import {NgModule} from '@angular/core'
 import {FormsModule} from '@angular/forms'
 import {HttpModule} from '@angular/http'
 import {MaterialModule} from '@angular/material'
+import {MomentModule} from 'angular2-moment'
 
 import {AppComponent} from './app.component'
 import {ListComponent} from './list/list.component'
 
 import {AppRoutingModule} from './app.routing'
-import { MenuComponent } from './menu/menu.component'
-import { DetailComponent } from './detail/detail.component'
+import {MenuComponent} from './menu/menu.component'
+import {DetailComponent} from './detail/detail.component'
 
 @NgModule({
     declarations: [
@@ -23,7 +24,8 @@ import { DetailComponent } from './detail/detail.component'
         FormsModule,
         HttpModule,
         AppRoutingModule,
-        MaterialModule.forRoot()
+        MaterialModule.forRoot(),
+        MomentModule
     ],
     providers: [
     ],
