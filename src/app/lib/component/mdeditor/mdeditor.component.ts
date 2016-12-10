46bb8c6bf4924a8467b96628253de47dcff62f0b
WittBulter <nanazuimeng123@gmail.com>
2016-12-11 00:53:46 +0800

    为md组件增加逻辑

--- src/app/lib/component/mdeditor/mdeditor.component.ts
+++ src/app/lib/component/mdeditor/mdeditor.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, ViewChild, Input, AfterViewInit} from '@angular/core'
+
+declare let SimpleMDE: any
 
 @Component({
   selector: 'app-mdeditor',
   templateUrl: './mdeditor.component.html',
   styleUrls: ['./mdeditor.component.scss']
 })
-export class MdeditorComponent implements OnInit {
+export class MdeditorComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  @ViewChild('simplemde') textarea : ElementRef
+
+  @Input() text: string;
+
+  ngAfterViewInit() {
+    let mde = new SimpleMDE({
+      element: this.textarea.nativeElement,
+      showIcons: ["code", "table"]
+    });
   }
 
 }
