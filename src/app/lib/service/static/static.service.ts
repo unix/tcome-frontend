d8d475c5853b6a22f59c1273a130e296519690b2
WittBulter <nanazuimeng123@gmail.com>
2016-12-10 22:49:03 +0800

    perfect function

--- src/app/lib/service/static/static.service.ts
+++ src/app/lib/service/static/static.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core'
 
 @Injectable()
 export class StaticService {
 
-  constructor() { }
+    constructor() {
+    }
+
+    private server: string = 'http://admin.itsmycar.cn/'
+
+    public makeApi(path: string) {
+        return this.server + path
+    }
 
 }
