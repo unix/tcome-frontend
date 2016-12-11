6a7fcbd17d08d471cfe70fed4737004f3d6bb7de
WittBulter <nanazuimeng123@gmail.com>
2016-12-11 21:49:12 +0800

    登出逻辑

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,18 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {StaticService} from '../lib/service/static'
+import {MenuService} from './menu.service'
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.scss']
+    styleUrls: ['./menu.component.scss'],
+    providers: [StaticService, MenuService]
 })
 export class MenuComponent implements OnInit {
 
-    constructor (){
-
+    constructor (
+        private menuService: MenuService
+    ){
     }
 
     logout (){
+            this.menuService.logout()
+                .subscribe(
+                    res => {
 
+                    },
+                    error => {}
+                )
     }
 
     ngOnInit (){
--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core'
-import {Http, Response, Headers, RequestOptions} from '@angular/http'
+import {Http} from '@angular/http'
 import {Observable} from 'rxjs/Observable'
 
 import {StaticService} from '../lib/service/static'
@@ -7,18 +7,16 @@ import {StaticService} from '../lib/service/static'
 @Injectable()
 export class MenuService {
 
-    constructor (
-        private http: Http,
-        private staticService: StaticService
-    ){
+    constructor (private http: Http,
+                 private staticService: StaticService){
     }
 
-    private detailUrl = this.staticService.makeApi('articles')
-    private options = new RequestOptions({
-        headers: new Headers({
-            'Content-Type': 'application/json',
-            'Authorization': this.staticService.authorization()
-        }),
-    })
+    private session = this.staticService.makeApi('session')
+
+    logout ():Observable<any> {
+        return this.http.delete(this.session, this.staticService.options)
+            .map(this.staticService.extractData)
+            .catch(this.staticService.handleError)
+    }
 
 }
