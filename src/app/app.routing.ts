06d5378015491224305e596730ec7b939b2085dd
WittBulter <nanazuimeng123@gmail.com>
2016-12-11 19:25:43 +0800

    优化路由

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,13 @@
 /**
  * Created by WittBulter on 16/8/17.
  */
+
 import {NgModule} from '@angular/core'
 import {Routes, RouterModule} from '@angular/router'
-import {ListComponent} from "./list/list.component";
+
+import {ListComponent} from "./list/list.component"
 import {DetailComponent} from './detail/detail.component'
+import {HomeComponent} from './home/home.component'
 
 
 /**
@@ -12,8 +15,10 @@ import {DetailComponent} from './detail/detail.component'
  * @type {{path: string; component: SessionComponent}[]}
  */
 const appRoutes: Routes = [
-    {path: '', component: ListComponent},
-    {path: 'article/:id', component: DetailComponent},
+    {path: '', component: HomeComponent},
+    {path: 'articles', component: ListComponent},
+    {path: 'articles/:id', component: DetailComponent},
+    {path: 'welcome', component: HomeComponent}
 ]
 
 /**
