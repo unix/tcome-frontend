45c46810ac03291b2e0b9b62767590b1e3fa3300
WittBulter <nanazuimeng123@gmail.com>
2016-12-11 21:28:36 +0800

    优化staticService

--- src/app/list/list.service.ts
+++ src/app/list/list.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core'
-import { Http, Response } from '@angular/http'
+import {Http, Response} from '@angular/http'
 import {Observable} from 'rxjs/Observable'
 import {Headers, RequestOptions} from '@angular/http'
 
@@ -9,31 +9,17 @@ import {StaticService} from '../lib/service/static'
 @Injectable()
 export class ListService {
 
-    constructor(
+    constructor (
         private http: Http,
-        private staticService: StaticService
-    ) {
+        private staticService: StaticService){
     }
 
     private listUrl = this.staticService.makeApi('articles')
 
-    getList(): Observable<List[]> {
+    getList (): Observable<List[]>{
         return this.http.get(this.listUrl)
-            .map(this.extractData)
-            .catch(this.handleError)
+            .map(this.staticService.extractData)
+            .catch(this.staticService.handleError)
     }
-
-    private extractData(res: Response) {
-        const body = res.json();
-        return body || {}
-    }
-
-    private handleError (error: any) {
-        const errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        console.error(errMsg);
-        return Observable.throw(errMsg)
-    }
-
 
 }
