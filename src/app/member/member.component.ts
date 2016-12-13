6bc1b067c2ef1f789b87db0200063e7025837ac0
WittBulter <nanazuimeng123@gmail.com>
2016-12-14 00:14:16 +0800

    为会员中心加入按钮

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-member',
-  templateUrl: './member.component.html',
-  styleUrls: ['./member.component.scss']
+    selector: 'app-member',
+    templateUrl: './member.component.html',
+    styleUrls: ['./member.component.scss']
 })
 export class MemberComponent implements OnInit {
 
-  constructor() { }
+    constructor (){
+    }
 
-  ngOnInit() {
-  }
+    public tabbar:any[] = [{
+        content: '文章',
+        t: true
+    }, {
+        content: '回复',
+        t: false
+    }, {
+        content: '撰写',
+        t: false
+    },{
+        content: '更多',
+        t: false
+    }]
+
+    tabbarToggle (index){
+        this.tabbar = this.tabbar.map(v => ({t: false, content: v.content}))
+        this.tabbar[index].t = true
+    }
+
+    ngOnInit (){
+    }
 
 }
