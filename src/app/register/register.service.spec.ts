08f263c8e40792c451e9d5ad41331298e03300b5
WittBulter <nanazuimeng123@gmail.com>
2016-12-13 22:38:21 +0800

    添加注册服务

---
+++ src/app/register/register.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { RegisterService } from './register.service';
+
+describe('RegisterService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RegisterService]
+    });
+  });
+
+  it('should ...', inject([RegisterService], (service: RegisterService) => {
+    expect(service).toBeTruthy();
+  }));
+});
