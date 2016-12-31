07d836ecfc0ddb7dbdc1164d119ebfe7a98b7b7a
WittBulter <nanazuimeng123@gmail.com>
2017-01-01 00:36:59 +0800

    更新列表模型

--- src/app/list/list.ts
+++ src/app/list/list.ts
@@ -10,5 +10,6 @@ export class List {
         public createdAt: string,   // 创建日期
         public readTotal: number,   // 阅读数
         public commentTotal: number,// 评论数
+        public authorName: string,  // 用户姓名
     ){}
 }
