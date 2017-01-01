06d7a22fec206b5859322875dc229dd488f2c91a
WittBulter <nanazuimeng123@gmail.com>
2017-01-01 19:55:42 +0800

    增加titleService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, AfterViewInit, ViewChild} from '@angular/core'
+import {Title} from '@angular/platform-browser'
+
 import {MissionService} from './lib/service/mission'
 import {StaticService} from './lib/service/static'
 
@@ -11,6 +13,7 @@ import {StaticService} from './lib/service/static'
 export class AppComponent{
     constructor (
         private missionService: MissionService,
+        private titleService: Title,
     ){
         // 订阅子组件通知 通知至所有子组件
         missionService.missionConfirmed$.subscribe(
@@ -21,6 +24,6 @@ export class AppComponent{
     }
 
     ngAfterViewInit (){
-
+        this.titleService.setTitle('欢迎-维特博客')
     }
 }
