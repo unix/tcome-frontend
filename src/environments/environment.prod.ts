84afb666b407b8e71f54ce7a97ad6cf1f39dd383
WittBulter <nanazuimeng123@gmail.com>
2017-01-01 19:13:15 +0800

    更新服务端端口

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
+
     production: true,
-    host: 'http://120.27.200.112/v1/'
+
+    // 生产环境请反向代理较高的端口，API服务端不要直接使用80端口
+    host: 'http://120.27.200.112:3001/v1/'
 };
