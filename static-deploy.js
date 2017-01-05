61ae8d7e346db7b1cf148c3a3dd1d77d155aedb4
WittBulter <nanazuimeng123@gmail.com>
2017-01-05 20:51:51 +0800

    修复url替换错误的问题

--- static-deploy.js
+++ static-deploy.js
@@ -35,10 +35,10 @@ fs.readFile('./dist/index.html', (err, bufferData) =>{
 		return console.log('读取文件错误: 可能需要sudo权限');
 	}
 	let html = bufferData.toString();
-	html = html.replace(`src="/inline`, `src="http://static.wittsay.cc/inline`)
-	html = html.replace(`src="/vendor`, `src="http://static.wittsay.cc/vendor`)
-	html = html.replace(`src="/main`, `src="http://static.wittsay.cc/main`)
-	html = html.replace('href="styles', 'src="http://static.wittsay.cc/styles')
+	html = html.replace('src="inline', 'src="http://static.wittsay.cc/inline')
+	html = html.replace('src="vendor', 'src="http://static.wittsay.cc/vendor')
+	html = html.replace('src="main', 'src="http://static.wittsay.cc/main')
+	html = html.replace('href="styles', 'href="http://static.wittsay.cc/styles')
 
 	fs.writeFile('./dist/index.html', new Buffer(html), (err) =>{
 		if (err) return console.log(err);
