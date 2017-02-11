d890118dfac1b5bd46f186336203ce107206ba8a
WittBulter <nanazuimeng123@gmail.com>
2017-02-11 21:24:20 +0800

    增加设备判断

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {RouterModule} from '@angular/router'
 import {MomentModule} from 'angular2-moment'
 import {ToastyModule} from 'ng2-toasty';
 import {SharedModule} from './shared/shared.module'
+import {ResponsiveModule, ResponsiveConfig} from 'ng2-responsive'
 
 import {AppComponent} from './app.component'
 import {AppRoutingModule} from './app.routing'
@@ -17,9 +18,25 @@ import {AuthService} from './shared/service/auth'
 import {AdminService} from './shared/service/admin'
 import {LockerService} from './shared/service/locker'
 
+const config = {
+    breakPoints: {
+        xs: {max: 600},
+        sm: {min: 601, max: 959},
+        md: {min: 960, max: 1279},
+        lg: {min: 1280, max: 1919},
+        xl: {min: 1920}
+    },
+    debounceTime: 100
+};
+
+export function ResponsiveDefinition (){
+    return new ResponsiveConfig(config);
+}
+
 @NgModule({
     imports: [
         ToastyModule.forRoot(),
+        ResponsiveModule,
         BrowserModule,
         CommonModule,
         FormsModule,
@@ -38,7 +55,11 @@ import {LockerService} from './shared/service/locker'
     providers: [
         LockerService,
         AuthService,
-        AdminService
+        AdminService,
+        {
+            provide: ResponsiveConfig,
+            useFactory: ResponsiveDefinition
+        }
     ],
     bootstrap: [AppComponent],
 })
